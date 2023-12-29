074ba74ac3fd8db36331b672c8fc2481b35b1b38
Al West <al@tsew.net>
2023-12-29 18:26:53 +0000

    First commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use a base image with rsync installed
+FROM alpine:latest
+
+# Install rsync and other utilities
+RUN apk add --no-cache rsync bash
+
+# Create a directory for the scripts and volumes for the mirror data
+RUN mkdir /scripts
+VOLUME ["/mirror"]
+
+# Copy the sync script into the container
+COPY sync-mirrors.sh /scripts/
+
+# Make sure the script is executable
+RUN chmod +x /scripts/sync-mirrors.sh
+
+# This script will be executed when the container starts
+ENTRYPOINT ["/scripts/sync-mirrors.sh"]
+
---
+++ sync-mirrors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true; do
+    # Sync CentOS 7
+#    mkdir -p /mirror/centos
+ #   rsync -avSHP --delete rsync://mirror.liquidtelecom.com/centos/7/ /mirror/centos/7/
+
+    # Sync Rocky Linux 8
+#'    mkdir -p /mirror/rocky
+#    rsync -avSHP --delete rsync://mirror.netzwerge.de/rocky-linux/8/ /mirror/rocky/8/
+
+    # Sync Rocky Linux 9
+    rsync -avSHP --delete rsync://mirror.netzwerge.de/rocky-linux/9/ /mirror/rocky/9/
+
+    # Add any other sync commands needed here
+
+    echo "Sync completed. Waiting for 6 hours before the next sync."
+    
+    # Wait for 6 hours (6 hours * 60 minutes * 60 seconds)
+    sleep 21600
+done
+
